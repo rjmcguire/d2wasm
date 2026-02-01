bb075071d48f16c97ee08fdac34d6d01abf67668
Rory McGuire <rjmcguire@gmail.com>
2026-02-01 23:01:55 +0000

    verify string is part of output

--- tests/milestones/milestone_14_string_concat_chain/test.d
+++ tests/milestones/milestone_14_string_concat_chain/test.d
@@ -9,3 +9,24 @@ void printIt() {
     __writeln(D);
 }
 enum _ = printIt();
+
+// TODO: Other array concat tests (uncomment to test)
+// These should work with the same codegen once array support is added
+
+// int array concat
+// enum intArr1 = [1, 2, 3];
+// enum intArr2 = [4, 5];
+// enum intArr3 = intArr1 ~ intArr2;  // Should be [1, 2, 3, 4, 5]
+
+// Append element to array
+// enum withExtra = intArr1 ~ 4;  // Should be [1, 2, 3, 4]
+
+// char array (different from string?)
+// enum chars1 = ['a', 'b'];
+// enum chars2 = ['c', 'd'];
+// enum chars3 = chars1 ~ chars2;
+
+// ubyte array (common for binary data)
+// enum bytes1 = [cast(ubyte)0x01, cast(ubyte)0x02];
+// enum bytes2 = [cast(ubyte)0x03];
+// enum bytes3 = bytes1 ~ bytes2;
