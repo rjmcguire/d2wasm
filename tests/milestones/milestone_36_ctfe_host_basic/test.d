eb6eee9fa48872dfa976045952af2dab586c52c0
Rory McGuire <rjmcguire@gmail.com>
2026-02-02 16:46:07 +0000

    more ctfe work

--- tests/milestones/milestone_36_ctfe_host_basic/test.d
+++ tests/milestones/milestone_36_ctfe_host_basic/test.d
@@ -1,8 +1,9 @@
 import __ctfe_runtime;
 
 int compute() {
-    auto ptr = __ctfe_runtime.alloc(100);
-    return ptr > 0 ? 1 : 0;
+    // Allocate 100 bytes, should return pointer starting at MEMORY_RESERVED (1024)
+    int ptr = __ctfe_runtime.alloc(100);
+    return ptr;
 }
 
 enum allocated = compute();
